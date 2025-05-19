0e1dc1b19786a92f9071fe9b6723f0864b3be59a
hadiseh-a <hadisea82@gmail.com>
2025-05-19 22:46:58 +0330

    express tutorial added and done

---
+++ express-tutorial/src/index.mjs
@@ -0,0 +1,104 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "jack", displayName: "Jack" },
+  { id: 3, username: "tina", displayName: "Tina" },
+  { id: 4, username: "jason", displayName: "Jason" },
+  { id: 5, username: "henry", displayName: "Henry" },
+  { id: 6, username: "marilyn", displayName: "Marilyn" },
+  { id: 7, username: "adam", displayName: "Adam" },
+];
+
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "HELLO" });
+});
+
+app.get("/api/users", (request, response) => {
+  console.log(request.query);
+  const {
+    query: { filter, value },
+  } = request;
+  if (filter && value)
+    return response.send(
+      mockUsers.filter((user) => {
+        return user[filter].includes(value);
+      })
+    );
+  return response.send(mockUsers);
+});
+
+app.post("/api/users", (request, response) => {
+  const { body } = request;
+  console.log(request.body);
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return response.status(201).send(newUser);
+});
+
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedID = parseInt(request.params.id);
+  console.log(parsedID);
+  if (isNaN(parsedID))
+    return response.status(400).send({ msg: "Bad Request. Inavalid ID." });
+  const findUser = mockUsers.find((user) => user.id === parsedID);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+});
+
+app.put("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+  const parsedID = parseInt(id);
+  if (isNaN(parsedID)) return response.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID);
+
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers[findUserIndex] = { id: parsedID, ...body };
+  return response.sendStatus(200);
+});
+
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+  const parsedID = parseInt(id);
+  if (isNaN(parsedID)) return response.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID);
+
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
+});
+
+app.delete("/api/users/:id", (request, response) => {
+  const {
+    params: { id },
+  } = request;
+  const parsedID = parseInt(id);
+  if (isNaN(parsedID)) return response.send(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1)
+  return response.sendStatus(200);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
