7f058ef89cf74799cc52e5cc2173d2b6119c8155
Hadiseh A <154057381+hadiseh-a@users.noreply.github.com>
2025-07-30 12:51:33 +0330

    Update members.controller.js

--- mongodb-homework/server/controllers/members.controller.js
+++ mongodb-homework/server/controllers/members.controller.js
@@ -4,7 +4,6 @@ export const getAllFacultyMembers = async (req, res) => {
   try {
     const members = await Member.find({});
     res.status(200).json(members);
-    Member.fi
   } catch (error) {
     res.status(400).json({ msg: error.message });
   }
